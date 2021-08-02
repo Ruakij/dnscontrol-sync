b7dbe74d759b6de753c6cc00c1daf35bcffb52a7
Ruakij <alexander.b@railduction.eu>
2021-08-02 12:46:27 +0200

    Added example dnscontrol.js

---
+++ dnscontrol.js
@@ -0,0 +1,23 @@
+// Providers
+var REG_NONE = NewRegistrar('none', 'NONE');    // No registrar.
+
+var cloudflare = NewDnsProvider('cloudflare', 'CLOUDFLAREAPI');
+
+
+// Default settings
+DEFAULTS(
+	NAMESERVER_TTL('1d'),
+	DefaultTTL('1d'),
+	AUTOSPLIT,              // Split long TXT
+	CF_PROXY_DEFAULT_OFF    // Dont use Cloudflare-Proxy-Feature
+);
+
+
+// Zone-declaration
+D('example.com', REG_NONE, DnsProvider(cloudflare));
+
+
+
+// Include
+//  (Do not touch unless you know what you are doing!)
+require_glob("");   // TODO: determine correct location to include
